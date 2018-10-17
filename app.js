9c012d85ece2aecc066d4ee8983b6c3882cfaeb4
Jason <haloos@me.com>
2018-10-17 19:56:11 -0400

    added console.time,clear,warn,timeEnd

--- app.js
+++ app.js
@@ -7,3 +7,14 @@ console.log(greeting);
 console.log([1,2,3,4,5]);
 console.log({a:1, b:2});
 console.table({a:1, b:2});
+
+console.error('This is and error');
+console.clear();
+console.warn('This is a warning');
+console.time('Hello');
+console.log('Hello World');
+console.log('Hello World');
+console.log('Hello World');
+console.log('Hello World');
+console.log('Hello World');
+console.timeEnd('Hello');
