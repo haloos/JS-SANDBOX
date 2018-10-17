da048cb06e8fb6778eb9762737a6c72f133dfc1f
Jason <haloos@me.com>
2018-10-17 19:42:08 -0400

    first commit

---
+++ app.js
@@ -0,0 +1,9 @@
+// Log to console
+console.log('Hello World');
+console.log(123);
+console.log(true);
+var greeting = 'Hello';
+console.log(greeting);
+console.log([1,2,3,4,5]);
+console.log({a:1, b:2});
+console.table({a:1, b:2});
