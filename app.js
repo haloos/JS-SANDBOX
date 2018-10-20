0ebf227b4d9a261475b82b52250860ebb1e7a239
Jason <haloos@me.com>
2018-10-19 21:09:49 -0400

    Simple math numbers, objects

--- app.js
+++ app.js
@@ -1,26 +1,28 @@
-// Primitive
+const num1 = 100; 
+const num2 = 50;
+let val;
 
-// String
-const name = 'John Doe';
-// Number
-const age = 55;
-// Boolean
-const hasKids = true;
-// Null
- const car = null;
- // undefined 
-let test;
-// Symbol
-const sym = Symbol();
+// Simple math with numbers
+val = num1 + num2;
+val = num1 * num2;
+val = num1 - num2;
+val = num1 / num2;
+val = num1 % num2;
 
-// Reference Types - Objects
-// Array
-const hobbies = ['movies', 'music'];
-// Object litteral
-const address =  {
-  city: 'NYC',
-  state: 'NY',
-}
-const today = new Date();
-console.log(today);
-console.log(typeof today);
+// Math Object
+val = Math.PI;
+val = Math.E;
+val = Math.round(2.8);
+val = Math.ceil(2.4);
+val = Math.floor(2.8);
+val = Math.sqrt(64);
+val = Math.abs(-3);
+val = Math.pow(8, 2);
+val = Math.min(2,33,4,1,55,6,-2);
+val = Math.min(2,33,4,1,55,6,-2);
+val = Math.random()
+
+val = Math.floor(Math.random() * 20 + 1);
+
+
+console.log(val);
