2c6aa396062dcd7c25b010f95c40eda3de088210
Jason <haloos@me.com>
2018-10-20 14:05:54 -0400

    Type conversion examples

--- app.js
+++ app.js
@@ -1,28 +1,44 @@
-const num1 = 100; 
-const num2 = 50;
 let val;
 
-// Simple math with numbers
-val = num1 + num2;
-val = num1 * num2;
-val = num1 - num2;
-val = num1 / num2;
-val = num1 % num2;
+// Number to string
+val = String(5);
+val = String(4+4);
 
-// Math Object
-val = Math.PI;
-val = Math.E;
-val = Math.round(2.8);
-val = Math.ceil(2.4);
-val = Math.floor(2.8);
-val = Math.sqrt(64);
-val = Math.abs(-3);
-val = Math.pow(8, 2);
-val = Math.min(2,33,4,1,55,6,-2);
-val = Math.min(2,33,4,1,55,6,-2);
-val = Math.random()
+// Boolean to string
+val = String(true);
 
-val = Math.floor(Math.random() * 20 + 1);
+// Date to string
+val = String(new Date());
+
+// Array to string
+val = String([1,2,3,4,5]);
 
 
-console.log(val);
+// toString()
+val = (5).toString();
+val = (true).toString();
+
+// String to number
+val = Number('5');
+val = Number(true);
+val = Number(false);
+val = Number(null);
+val = Number('hello');
+val = Number([1,2,3,]);
+
+val = parseInt('100');
+val = parseFloat('100.3');  // parseFloat to use decimals 
+
+
+//Output
+//console.log(val);
+//console.log(typeof val);
+//console.log(val.length);
+//console.log(val.toFixed());
+
+const val1 = 5;
+const val2 = 6;
+const sum = val1 + val2;
+
+console.log(sum);
+console.log(typeof sum);
