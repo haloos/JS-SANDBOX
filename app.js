f1a0201d80cf5f39ae0fe21e03a2551d5e351318
Jason <haloos@me.com>
2018-10-20 20:14:00 -0400

    Strings Concat, property and methods for strings

--- app.js
+++ app.js
@@ -1,44 +1,61 @@
+const firstName = 'William';
+const lastName = 'Johnson';
+const age = 36;
+const str = 'Hello there my name is Jason';
+const tags = 'web design, web development,programming';
 let val;
 
-// Number to string
-val = String(5);
-val = String(4+4);
+val = firstName + lastName;
+ 
+// Concatenation
+val = firstName + ' ' + lastName;
 
-// Boolean to string
-val = String(true);
+// Append
+val = 'Jason ';
+val += 'Paredes';
 
-// Date to string
-val = String(new Date());
+val = 'Hello my name is ' + firstName + ' and I am ' + age;
 
-// Array to string
-val = String([1,2,3,4,5]);
+// Escaping
+val = 'That\'s awesome I can\'t wait'; 
 
+// length 
+val = firstName.length;
 
-// toString()
-val = (5).toString();
-val = (true).toString();
+// concat
+val = firstName.concat(' ' , lastName);
 
-// String to number
-val = Number('5');
-val = Number(true);
-val = Number(false);
-val = Number(null);
-val = Number('hello');
-val = Number([1,2,3,]);
+// Change case
+val = firstName.toUpperCase();
+val = firstName.toLowerCase();
 
-val = parseInt('100');
-val = parseFloat('100.3');  // parseFloat to use decimals 
+val = firstName[2];
 
+// indexof()
+val = firstName.indexOf('l');        // from left to right 0,1,2
+val = firstName.lastIndexOf('l');    // from right to left 0,1,2
 
-//Output
-//console.log(val);
-//console.log(typeof val);
-//console.log(val.length);
-//console.log(val.toFixed());
+// charAt()
+val = firstName.charAt('2');
 
-const val1 = 5;
-const val2 = 6;
-const sum = val1 + val2;
+// Get last char 
+val = firstName.charAt(firstName.length -1);
 
-console.log(sum);
-console.log(typeof sum);
+// substring ()
+val = firstName.substring(0, 4);
+
+// slice()  mostly used with arrays
+val = firstName.slice(0,4);
+val = firstName.slice(-3);  // last 3 from string  iam
+
+// split()
+val = str.split(' ');
+val = tags.split(',');
+
+// replace()
+val = str.replace('Jason', 'Jack');
+
+// includes()
+val = str.includes('Hello');
+console.log(val);
+
