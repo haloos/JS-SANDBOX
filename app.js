d08bf720af22d7f936ebed652a72d1412266caf9
Jason <haloos@me.com>
2018-10-21 12:45:57 -0400

    Creating arrays with helpful methods

--- app.js
+++ app.js
@@ -1,40 +1,7 @@
-const person = {
-  firstName: 'Steve',
-  lastName: 'Smith',
-  age : 30,
-  email: 'steve@aol.com',
-  hobbies: ['music', 'sports'],
-  address: {
-    city: 'Miami',
-    state: 'FL'
-  },
-  getBirthYear: function(){
-   return 2018 - this.age;  // sub tracts what in age
-  }  
-}
+ // Create some arrays 
+const numbers = [43,56,33,23,36,5];  // Most popular way
+const numbers2 = new Array(22,45,33,76,54);
+const fruit = ['Apple', 'Orange', 'Pear'];
+const mixed = [23, 'Hello', true, undefined, null, {a:1, b:1}, new Date() ];
 
-let val;
-
-val = person;
-// Get specific value
-val = person.firstName;  // recommed way
-val = person['lastName'];
-val = person.age;
-val = person.hobbies[1];
-val = person.address.state;
-val = person.address['city'];
-val = person.getBirthYear();
-
-
-console.log(val);
-
-
-const people  = [
-  {name: 'John', age: 30},
-  {name: 'Mike', age: 23},
-];
-
-for(let i = 0; i < people.length; i++){
-  console.log(people[i].name);
-}
-
+console.log(mixed);
