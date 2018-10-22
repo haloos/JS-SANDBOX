9265dc0ec8b14d6eaf4b15036ed1e4f0a7585a77
Jason <haloos@me.com>
2018-10-21 22:58:56 -0400

    If statement, if else statement

--- app.js
+++ app.js
@@ -1,24 +1,109 @@
-let val; 
+//if(something){
+ // do something
+//} else {
+  //do something else
+//}
 
-const today = new Date();     
-let birthday = new Date('9-10-1981 11:25:00');  // line 4,5,6 good examples of adding birthdays
-birthday = new Date('September 10 1981');
-birthday = new Date('9/10/1981');
+const id = 100;
 
-val = today.getMonth();   // Month starts at zero
-val = today.getDate();
-val = today.getDay();
-val = today.getFullYear();
-val = today.getHours();
-val = today.getMinutes();
-val = today.getSeconds();
-val = today.getMilliseconds();
-val = today.getTime();
+// Equal To
+/*if(id == 100) {                   // Not reconmmend 2 ==
+  console.log('Correct');         // === is the recommend way
+} else {
+  console.log('Incorrect');
+}
 
-birthday.getMonth(2);
-birthday.setDate(12);
-birthday.setFullYear(1985);
-birthday.setHours(3);
-birthday.setMinutes(30);
-birthday.setSeconds(25);
-console.log(birthday);
+// Not equal to
+if(id != 101){
+  console.log('Correct');
+} else {
+  console.log('Incorrect');
+} 
+// Equal to the vaule and type
+if(id === 100) {                   
+  console.log('Correct');
+} else {
+  console.log('Incorrect');
+}
+
+// Equal to the vaule and type
+if(id !== 100) {                // !== not equal    
+  console.log('Correct');
+} else {
+  console.log('Incorrect');
+}*/
+
+// Test if undefined
+// if(typeof id !== 'undefined'){     // line 36 way to test something underfined
+  //consold.log(`The id is ${id}`);
+// } else {
+ // console.log('no id');
+// }
+
+// Greater than or less than
+//if(id > 200) {                
+  //console.log('Correct');
+//} else {
+  //console.log('Incorrect');
+//}
+
+// If else 
+
+const color = 'yellow';
+
+//if(color === 'red') {
+  //console.log('color is red');
+//} else if(color === 'blue'){
+  //console.log('color is blue');
+//} else {
+  //console.log('color is not red or blue');
+//}
+
+
+// Logical operator
+const name = 'Steve';
+const age = 40;
+
+// And &&
+if(age > 0 && age < 12){
+  console.log(`${name} is a child`);
+} else if(age >= 13 && age <=19){
+  console.log(`${name} is a teenager`);
+} else {
+  console.log(`${name} is an adult`);
+}
+
+// Or || 
+if(age < 16 || age > 65){
+  console.log(`${name} can not run in race`);
+}else{
+  console.log(`${name} is registed for the race`);
+}
+
+// Ternary operator 
+console.log(id === 100 ? 'Correct' : 'Incorrect');  // much easier and prefered way
+
+// WITHOUT BRACES
+if(id === 100)
+  console.log('CORRECT');
+  else 
+  console.log('INCORRECT');
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
