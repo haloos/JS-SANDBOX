84b901b2783716d525ad8c91f532c69911bf6291
Jason Paredes <haloos@me.com>
2018-10-22 20:47:11 -0400

    Switch examples

--- app.js
+++ app.js
@@ -1,101 +1,42 @@
-//if(something){
- // do something
-//} else {
-  //do something else
-//}
+const color = 'red';
 
-const id = 100;
-
-// Equal To
-/*if(id == 100) {                   // Not reconmmend 2 ==
-  console.log('Correct');         // === is the recommend way
-} else {
-  console.log('Incorrect');
+switch(color){
+  case 'red':
+    console.log('Color is red');
+    break;
+  case 'blue':
+    console.log('Color is blue');
+    break;
+  default:
+    console.log('Color is not red or blue');
+    break;
 }
 
-// Not equal to
-if(id != 101){
-  console.log('Correct');
-} else {
-  console.log('Incorrect');
-} 
-// Equal to the vaule and type
-if(id === 100) {                   
-  console.log('Correct');
-} else {
-  console.log('Incorrect');
+let day;
+switch (new date().getDay()) {
+  case 0:
+    day = "Sunday";
+    break;
+  case 1:
+    day = "Monday";
+    break;
+  case 2:
+    day = "Tuesday";
+    break;
+  case 3:
+    day = "Wensday";
+    break;
+  case 4:
+    day = "Thursday";
+    break;
+  case 5:
+    day = "Friday";
+    break;
+  case 6:
+    day = "Saturday";
+    break;
 }
-
-// Equal to the vaule and type
-if(id !== 100) {                // !== not equal    
-  console.log('Correct');
-} else {
-  console.log('Incorrect');
-}*/
-
-// Test if undefined
-// if(typeof id !== 'undefined'){     // line 36 way to test something underfined
-  //consold.log(`The id is ${id}`);
-// } else {
- // console.log('no id');
-// }
-
-// Greater than or less than
-//if(id > 200) {                
-  //console.log('Correct');
-//} else {
-  //console.log('Incorrect');
-//}
-
-// If else 
-
-const color = 'yellow';
-
-//if(color === 'red') {
-  //console.log('color is red');
-//} else if(color === 'blue'){
-  //console.log('color is blue');
-//} else {
-  //console.log('color is not red or blue');
-//}
-
-
-// Logical operator
-const name = 'Steve';
-const age = 40;
-
-// And &&
-if(age > 0 && age < 12){
-  console.log(`${name} is a child`);
-} else if(age >= 13 && age <=19){
-  console.log(`${name} is a teenager`);
-} else {
-  console.log(`${name} is an adult`);
-}
-
-// Or || 
-if(age < 16 || age > 65){
-  console.log(`${name} can not run in race`);
-}else{
-  console.log(`${name} is registed for the race`);
-}
-
-// Ternary operator 
-console.log(id === 100 ? 'Correct' : 'Incorrect');  // much easier and prefered way
-
-// WITHOUT BRACES
-if(id === 100)
-  console.log('CORRECT');
-  else 
-  console.log('INCORRECT');
-
-
-
-
-
-
-
-
+console.log(`Today is ${day}`);
 
 
 
