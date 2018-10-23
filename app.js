c3c01e3fa0a135d6117e38d8f128738ab4b5170a
Jason <haloos@me.com>
2018-10-23 12:41:47 -0400

    For loop, contine, break examples

--- app.js
+++ app.js
@@ -3,9 +3,15 @@
 for(let i = 0; i < 10; i++){
   //console.log('Number ' + i);
   if(i === 2){
-    console.log('2 is my favorite number');
+    console.log('2 is my favorite number'); // add words into the loop(keeps number2)
+    continue;   // "Stops loop here without conting next interation" (no number2)
   }
-  console.log('Number '+ i);
+
+  if(i === 5){
+    console.log('Stop the loop');
+    break;
+  }
+  console.log('Number '+ i);   // "Really important statements to learn"
 }
 
 
