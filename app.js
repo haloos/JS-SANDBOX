a67befea85976112306e5b8acbbd3df6f5fd35e7
Jason Paredes <haloos@me.com>
2018-10-22 21:44:36 -0400

    Function Declarations & Expressions

--- app.js
+++ app.js
@@ -1,7 +1,42 @@
 // FUNCTION DECLARATIONS
 
-function greet(){
-  console.log('Hello');
+function greet(firstName = 'John' , lastName = 'Doe'){  // Best way for functions
+  //console.log('Hello');
+  return 'Hello ' + firstName + ' ' + lastName;
 }
 
-greet();
+//console.log(greet('John', 'Doe'));
+
+// FUNCTION EXPRESSIONS
+
+const square = function(x){
+  return x*x;
+};
+//console.log(square(8));
+
+// IMMIDIATLEY INVOKABLE FUNCTION EXPRESSIONS - IIFEs
+
+//(function() {
+  //console.log("IIFE Ran..."); // must add (); for IIFEs to work
+//})();
+
+//(function(name) {
+  //console.log('Hello '+ name);    // Very useful for certan design patterns
+//})('Jason');
+
+// PROPERTY METHODS
+
+const todo = {
+    add: function(){
+      console.log('add todo..');
+    }, edit: function(id){
+      console.log(`edit todo ${id}`);
+    }
+}
+todo.delate = function(){
+  console.log('Delete todo...');
+}
+
+todo.add();
+todo.edit(22);
+todo.delate();
