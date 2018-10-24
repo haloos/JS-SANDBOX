299957d5588aff9471917d26ba7c8823aa93aea0
Jason <haloos@me.com>
2018-10-24 13:22:47 -0400

    Global scope, Block, var issues

--- app.js
+++ app.js
@@ -1,57 +1,46 @@
-//  WINDOW METHODS / OBJECTS / PROPERTIES
+// Global Scope
+var a = 1;
+let b = 2;
+const c = 3;
 
-// Alert
-//alert('Helo World');      // Recommend errs on form vaulidation (old school)
 
-// Prompt
-//const input = prompt();
-//alert(input);
+//function test(){            // Seperate Variable because of dif scope
+//var a = 4;
+//let b = 5;
+//const c = 6;
+//console.log('Function Scope: ', a, b, c,);
+//}
+//test();
 
-// Confirm
-//if(confirm('Are you sure')){
-  //console.log('YES');
- //} else {
-  //console.log('NO');
- // }
+//if(true) {  //Block Scope
+  // Need to watch var in block scope, they over ride the "Global scope" (security issues)
+//var a = 4;
+//let b = 5;
+//const c = 6;
+//console.log('If Scope: ', a, b, c,);
 
- let val;
+//}
 
- // Putter height and width
- val = window.outerHeight;
- val = window.outerWidth;
- 
- // Inner height and width
- val = window.innerHeight;
- val = window.innerWidth;
- 
- // Scroll points
- val = window.scroLLY;
- val = window.scroLLX;
- 
- 
- // Location Object
- val = window.location;
- val = window.location.hostname;
- val = window.location.port;
- val = window.location.href;
- val = window.location.search;
- 
- // Redirect
- //window.location.href = 'http://google.com';
- 
- // Reload 
- //window.location.reload();    // only usuable with a function 
- 
- // History Object 
- //window.history.go(-1);
- //val = window.history.length;
- 
- // Navigator Object
- val = window.navigator;
- val = window.navigator.appName;
- val = window.navigator.appVersion;
- val = window.navigator.userAgent;
- val = window.navigator.platform;
- val = window.navigato.vendor;
- val = window.navigator.language; 
- console.log(val);
+for(let a = 0; a < 10; a ++){
+console.log(`Loop: ${a}`);
+}
+
+console.log('Global Scope: ', a, b, c,);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
