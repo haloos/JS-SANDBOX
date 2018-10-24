472390bdff1fa3360be10da5e40a8d5dc8e24447
Jason Paredes <haloos@me.com>
2018-10-23 20:33:55 -0400

    Window methods, objects, properties

--- app.js
+++ app.js
@@ -1,42 +1,47 @@
-// FUNCTION DECLARATIONS
+//  WINDOW METHODS / OBJECTS / PROPERTIES
 
-function greet(firstName = 'John' , lastName = 'Doe'){  // Best way for functions
-  //console.log('Hello');
-  return 'Hello ' + firstName + ' ' + lastName;
-}
+// Alert
+//alert('Helo World');      // Recommend errs on form vaulidation (old school)
 
-//console.log(greet('John', 'Doe'));
+// Prompt
+//const input = prompt();
+//alert(input);
 
-// FUNCTION EXPRESSIONS
+// Confirm
+//if(confirm('Are you sure')){
+  //console.log('YES');
+ //} else {
+  //console.log('NO');
+ // }
 
-const square = function(x){
-  return x*x;
-};
-//console.log(square(8));
+let val;
 
-// IMMIDIATLEY INVOKABLE FUNCTION EXPRESSIONS - IIFEs
+// Putter height and width
+val = window.outerHeight;
+val = window.outerWidth;
 
-//(function() {
-  //console.log("IIFE Ran..."); // must add (); for IIFEs to work
-//})();
+// Inner height and width
+val = window.innerHeight;
+val = window.innerWidth;
 
-//(function(name) {
-  //console.log('Hello '+ name);    // Very useful for certan design patterns
-//})('Jason');
+// Scroll points
+val = window.scroLLY;
 
-// PROPERTY METHODS
+console.log(val);
 
-const todo = {
-    add: function(){
-      console.log('add todo..');
-    }, edit: function(id){
-      console.log(`edit todo ${id}`);
-    }
-}
-todo.delate = function(){
-  console.log('Delete todo...');
-}
 
-todo.add();
-todo.edit(22);
-todo.delate();
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
