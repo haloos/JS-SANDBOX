b3d18d3d743d279291a13d30ed08c0e302f6664a
Jason Paredes <haloos@me.com>
2018-10-23 20:36:50 -0400

    Window methods,Objects,Properties

--- app.js
+++ app.js
@@ -26,6 +26,44 @@ val = window.innerWidth;
 
 // Scroll points
 val = window.scroLLY;
+val = window.scroLLX;
+
+
+// Location Object
+val = window.location;
+val = window.location.hostname;
+val = window.location.port;
+val = window.location.href;
+val = window.location.search;
+
+// Redirect
+//window.location.href = 'http://google.com';
+
+// Reload 
+//window.location.reload();    // only usuable with a function 
+
+// History Object 
+//window.history.go(-1);
+//val = window.history.length;
+
+// Navigator Object
+val = window.navigator;
+val = window.navigator.appName;
+val = window.navigator.appVersion;
+val = window.navigator.userAgent;
+val = window.navigator.platform;
+val = window.navigato.vendor;
+val = window.navigator.language;
+
+
+
+
+
+
+
+
+
+
 
 console.log(val);
 
