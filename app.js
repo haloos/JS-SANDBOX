db974ab90df05b308c446d8e646454d146502455
Jason <haloos@me.com>
2018-10-26 19:59:13 -0400

    Traversing the DOM

--- app.js
+++ app.js
@@ -1,167 +1,54 @@
-// document.getElementsByClassName
-
-//const items = document.getElementsByClassName('collection-item');
-//console.log(items);
-//console.log(items[0]);
-//items[0].style.color = 'red';
-//items[3].textContent = 'Hello';
-
-//const listItems = document.querySelector('ul').getElementsByClassName('collection-item');
-
-//console.log(listItems);
-
-// document.getElementsByTagName
-//let lis = document.getElementsByTagName('li');
-//console.log(lis);
-//console.log(lis[0]);
-//lis[0].style.color = 'red';
-//lis[3].textContent = 'Hello';
-
-// Conver HTML Collection into an Array
-//lis = Array.from(lis);
-
-//lis.reverse();
-
-//lis.forEach(function(li, index){
-  //console.log(li.className);
- //li.textContent = `${index}:Hello`;
-//});
-
-// document.querySelectorAll
-
-const items = document.querySelectorAll('ul.collection li.collection-item');
-
-items.forEach(function(item, index){
-  item.textContent = `${index}:Hello`;
-});
-
-const liOdd = document.querySelectorAll('li:nth-child(odd');
-const liEven = document.querySelectorAll('li:nth-child(even');
-
-liOdd.forEach(function(li, index){
-  li.style.background = '#ccc';
-});
-
-for(let i = 0; i < liEven.length; i ++){
-  liEven[i].style.background = 'f4f4f4';
-}
-
-console.log(items);
-//console.log(lis);
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+let val;
+
+const list = document.querySelector('ul.collection');
+const listItem = document.querySelector('li.collection-item:first-child');
+
+val = listItem;
+val = list;
+
+// Get child nodes
+val = list.childNodes;
+val = list.childNodes[0];
+val = list.childNodes[0].nodeName;
+val = list.childNodes[3].nodeType;
+
+// 1 - Element
+// 2 - Attribute (deprecated)
+// 3 - Text node
+// 8 - Comment
+// 9 - Document itself
+// 10 - Doctype
+
+
+// Get children element nodes
+val = list.children;
+val = list.children[1];
+list.children[1].textContent = 'Hello';
+// Children of children
+list.children[3].children[0].id = 'test-link';
+val = list.children[3].children[0];
+
+// First child
+val = list.firstChild;
+val = list.firstElementChild;
+
+// Last child
+val = list.lastChild;
+val = list.lastElementChild;
+
+// Count child elements
+val = list.childElementCount;
+
+// Get parent node
+val = listItem.parentNode;
+val = listItem.parentElement;
+val = listItem.parentElement.parentElement;
+
+// Get next sibling
+val = listItem.nextSibling;
+val = listItem.nextElementSibling.nextElementSibling.previousElementSibling;
+
+// Get prev sibling
+val = listItem.previousSibling;
+val = listItem.previousElementSibling;
+console.log(val);
